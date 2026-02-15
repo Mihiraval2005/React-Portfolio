c81a1b81a93a34597edabed5db23e1899b392ab5
Mihir Raval <mihiraval2005@gmail.com>
2026-02-15 19:48:51 +0530

    Make Proper Email Formate

--- Server/controllers/contactController.js
+++ Server/controllers/contactController.js
@@ -22,13 +22,30 @@ export const createContact = async (req, res) => {
         await sendEmail({
             to: email,
             subject: `Thank you for contacting us, ${name}!`,
-            text: `Hi ${name},\n\n` +
-                `Thank you for reaching out. We have received your message:\n\n` +
-                `"${message}"\n\n` +
-                `We will get back to you as soon as possible.\n\n` +
-                `Best regards,\n` +
-                `Mihir Raval`,
+            html: `
+              <h1 style="color: #F87171; font-size: 24px; font-weight: 800; margin: 10px 0;text-align: center;">
+        Mihir.
+      </h1>
+    <div style="font-family: Arial, sans-serif; color: #333;">
+      <p>Hi ${name},</p>
+
+    
+
+      <p>Thank you for reaching out. We have received your message:</p>
+
+      <blockquote style="border-left: 3px solid #eee; padding-left: 10px; color: #555; margin: 10px 0;">
+        ${message}
+      </blockquote>
+
+      <p>We will get back to you as soon as possible.</p>
+
+      <p>Best regards,<br/>
+      <strong>Mihir Raval</strong></p>
+    </div>
+  `,
         });
+
+
 
 
         return res.status(201).json({ success: true, message: "Message sent successfully" });
