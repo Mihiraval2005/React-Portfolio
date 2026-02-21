4b9e532d59cbf4e0198ef28315c21a64ec9ddef4
Mihir Raval <mihiraval2005@gmail.com>
2026-02-22 00:31:41 +0530

    test

--- Server/config/email.js
+++ Server/config/email.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use TLS
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: 'mihiraval2005@gmail.com',
+    pass: 'upaomkxzfpksuptu',
   },
 });
 
